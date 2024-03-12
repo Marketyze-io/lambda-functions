dcaa0ff443e79d3e0333b087df2291fdd5b42dd2
SpawnageLoong <richardloongcj@gmail.com>
2024-03-12 15:41:57 +0700

    Add prototype modal

--- slack/slackInteractionsParser.py
+++ slack/slackInteractionsParser.py
@@ -10,196 +10,72 @@ headers = {
 }
 
 modal_bulkFbCampaigns = {
-    "type": "modal",
-    "submit": {
-      "type": "plain_text",
-      "text": "Submit",
-      "emoji": True
-    },
-    "close": {
-      "type": "plain_text",
-      "text": "Cancel",
-      "emoji": True
-    },
-    "title": {
-      "type": "plain_text",
-      "text": "Facebook Campaigns",
-      "emoji": True
-    },
-    "blocks": [
-      {
-        "type": "section",
-        "text": {
-          "type": "plain_text",
-          "text": ":wave: Hey David!\n\nWe'd love to hear from you how we can make this place the best place you’ve ever worked.",
-          "emoji": True
-        }
-      },
-      {
-        "type": "divider"
-      },
-      {
-        "type": "input",
-        "label": {
-          "type": "plain_text",
-          "text": "You enjoy working here at Pistachio & Co",
-          "emoji": True
-        },
-        "element": {
-          "type": "radio_buttons",
-          "options": [
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "Strongly agree",
-                "emoji": True
-              },
-              "value": "1"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "Agree",
-                "emoji": True
-              },
-              "value": "2"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "Neither agree nor disagree",
-                "emoji": True
-              },
-              "value": "3"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "Disagree",
-                "emoji": True
-              },
-              "value": "4"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "Strongly disagree",
-                "emoji": True
-              },
-              "value": "5"
-            }
-          ]
-        }
-      },
-      {
-        "type": "input",
-        "label": {
-          "type": "plain_text",
-          "text": "What do you want for our team weekly lunch?",
-          "emoji": True
-        },
-        "element": {
-          "type": "multi_static_select",
-          "placeholder": {
-            "type": "plain_text",
-            "text": "Select your favorites",
-            "emoji": True
-          },
-          "options": [
-            {
-              "text": {
-                "type": "plain_text",
-                "text": ":pizza: Pizza",
-                "emoji": True
-              },
-              "value": "value-0"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": ":fried_shrimp: Thai food",
-                "emoji": True
-              },
-              "value": "value-1"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": ":desert_island: Hawaiian",
-                "emoji": True
-              },
-              "value": "value-2"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": ":meat_on_bone: Texas BBQ",
-                "emoji": True
-              },
-              "value": "value-3"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": ":hamburger: Burger",
-                "emoji": True
-              },
-              "value": "value-4"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": ":taco: Tacos",
-                "emoji": True
-              },
-              "value": "value-5"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": ":green_salad: Salad",
-                "emoji": True
-              },
-              "value": "value-6"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": ":stew: Indian",
-                "emoji": True
-              },
-              "value": "value-7"
-            }
-          ]
-        }
-      },
-      {
-        "type": "input",
-        "label": {
-          "type": "plain_text",
-          "text": "What can we do to improve your experience working here?",
-          "emoji": True
-        },
-        "element": {
-          "type": "plain_text_input",
-          "multiline": True
-        }
-      },
-      {
-        "type": "input",
-        "label": {
-          "type": "plain_text",
-          "text": "Anything else you want to tell us?",
-          "emoji": True
-        },
-        "element": {
-          "type": "plain_text_input",
-          "multiline": True
-        },
-        "optional": True
-      }
-    ]
-  }
+	"type": "modal",
+	"submit": {
+		"type": "plain_text",
+		"text": "Submit",
+		"emoji": True
+	},
+	"close": {
+		"type": "plain_text",
+		"text": "Cancel",
+		"emoji": True
+	},
+	"title": {
+		"type": "plain_text",
+		"text": "Facebook Campaigns",
+		"emoji": True
+	},
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": ":wave: Hey David!\n\nHere's the info I need before I can create those campaigns for you.",
+				"emoji": True
+			}
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "input",
+			"element": {
+				"type": "url_text_input",
+				"action_id": "spreadsheet_url_input-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "Spreadsheet URL",
+				"emoji": True
+			}
+		},
+		{
+			"type": "input",
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "ad_acc_id_input-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "Ad Account ID",
+				"emoji": True
+			}
+		},
+		{
+			"type": "input",
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "token_input-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "Access Token",
+				"emoji": True
+			}
+		}
+	]
+}
 
 def lambda_handler(event, context):
     urlQueryString = event['body']
