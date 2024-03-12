53865f9c473ace873759580c51a14f9e4cb06068
SpawnageLoong <richardloongcj@gmail.com>
2024-03-12 17:01:25 +0700

    Add response message on form submission

--- slack/slackInteractionsParser.py
+++ slack/slackInteractionsParser.py
@@ -3,14 +3,16 @@ import urllib.parse as urlparse
 import requests
 
 SLACK_PUSH_VIEWS_ENDPOINT = "https://slack.com/api/views.push"
+SLACK_POST_MESSAGE_ENDPOINT = "https://slack.com/api/chat.postMessage"
 
 # HTTP Headers
 headers = {
-    "Authorization": "Bearer xoxb-241133470262-6786705625524-lM5i9Ider3NSERhR6wRSitDu",
+	"Authorization": "Bearer xoxb-241133470262-6786705625524-lM5i9Ider3NSERhR6wRSitDu",
 }
 
 modal_bulkFbCampaigns = {
 	"type": "modal",
+	"callback_id": "fbBulkCampaign-form",
 	"submit": {
 		"type": "plain_text",
 		"text": "Submit",
@@ -40,6 +42,7 @@ modal_bulkFbCampaigns = {
 		},
 		{
 			"type": "input",
+			"block_id": "spreadsheet_url_input",
 			"element": {
 				"type": "url_text_input",
 				"action_id": "spreadsheet_url_input-action"
@@ -52,6 +55,7 @@ modal_bulkFbCampaigns = {
 		},
 		{
 			"type": "input",
+			"block_id": "ad_acc_id_input",
 			"element": {
 				"type": "plain_text_input",
 				"action_id": "ad_acc_id_input-action"
@@ -64,6 +68,7 @@ modal_bulkFbCampaigns = {
 		},
 		{
 			"type": "input",
+			"block_id": "token_input",
 			"element": {
 				"type": "plain_text_input",
 				"action_id": "token_input-action"
@@ -78,25 +83,49 @@ modal_bulkFbCampaigns = {
 }
 
 def lambda_handler(event, context):
-    urlQueryString = event['body']
-    parsedUrl = urlparse.parse_qs(urlQueryString)
-    payload = json.loads(parsedUrl['payload'][0])
+	urlQueryString = event['body']
+	parsedUrl = urlparse.parse_qs(urlQueryString)
+	payload = json.loads(parsedUrl['payload'][0])
 
-    trigger_id = payload['trigger_id']
-    action = payload['actions'][0]['action_id']
+	match payload['type']:
+		case 'view_submission':
+			view_type = payload['view']['callback_id']
+			match view_type:
+				case 'fbBulkCampaign-form':
+					submission = payload['view']['state']['values']
+					spreadsheet_url = submission['spreadsheet_url_input']['spreadsheet_url_input-action']['value']
+					ad_acc_id = submission['ad_acc_id_input']['ad_acc_id_input-action']['value']
+					token = submission['token_input']['token_input-action']['value']
+					# Do something with the data
+					print(payload)
+					requests.post(SLACK_POST_MESSAGE_ENDPOINT, headers=headers, data={
+						# channel is hardcoded for now
+						"channel": "C06JA4QHGBC",
+						"text": f"Spreadsheet URL: {spreadsheet_url}\nAd Account ID: {ad_acc_id}\nAccess Token: {token}"
+					})
+					return {
+						'statusCode': 200
+					}
+				case _:
+					return json.dumps({
+						"statusCode": 200
+					})
+		
+		case 'block_actions':
+			trigger_id = payload['trigger_id']
+			action = payload['actions'][0]['action_id']
+			match action:
+				case 'button-bulk-fb-campaigns':
+					requests.post(SLACK_PUSH_VIEWS_ENDPOINT, headers=headers, data={
+						"trigger_id": trigger_id,
+						"view": json.dumps(modal_bulkFbCampaigns)
+					})
+				case _:
+					requests.post(SLACK_PUSH_VIEWS_ENDPOINT, headers=headers, data={
+						"trigger_id": trigger_id,
+						"view": json.dumps(modal_bulkFbCampaigns)
+					})
 
-    match action:
-        case 'button-bulk-fb-campaigns':
-            requests.post(SLACK_PUSH_VIEWS_ENDPOINT, headers=headers, data={
-                "trigger_id": trigger_id,
-                "view": json.dumps(modal_bulkFbCampaigns)
-            })
-        case _:
-            requests.post(SLACK_PUSH_VIEWS_ENDPOINT, headers=headers, data={
-                "trigger_id": trigger_id,
-                "view": json.dumps(modal_bulkFbCampaigns)
-            })
-
-    return {
-        'statusCode': 200
-    }
+	return {
+		'statusCode': 200
+	}
