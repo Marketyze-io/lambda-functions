3ca9baab37e81a955b133e39fdc2aa0e10663f7d
SpawnageLoong <richardloongcj@gmail.com>
2024-04-09 14:36:58 +0700

    Add support for single campaign creation requests

--- fbMarketing/createFbCampaign.py
+++ fbMarketing/createFbCampaign.py
@@ -2,6 +2,12 @@ import json
 import requests
 
 def lambda_handler(event, context):
+  is_single = False
+  if 'campaign_name' not in event:
+    is_single = True
+    event = json.loads(event['body'])
+    print("loading json...")
+    
   campaign_name = event['campaign_name']
   campaign_objective = event['campaign_objective']
   campaign_buying_type = event['campaign_buying_type']
@@ -9,6 +15,9 @@ def lambda_handler(event, context):
   special_ad_categories = event['special_ad_categories']
   access_token = event['access_token']
   ad_account_id = event['ad_account_id']
+  
+  if len(special_ad_categories) == 0:
+    special_ad_categories = ["NONE"]
 
   form_data = {
     'name': campaign_name,
@@ -19,7 +28,15 @@ def lambda_handler(event, context):
     'access_token': access_token
   }
 
-  url = f'https://graph.facebook.com/v19.0/act_{ad_account_id}/campaigns'
+  url = f'https://graph.facebook.com/v19.0/{ad_account_id}/campaigns'
   response = requests.post(url, data=form_data)
   response_data = response.json()
+    
+  if is_single:
+    return {
+      "isBase64Encoded": False,
+      "statusCode": 200,
+      "body": json.dumps(response_data)
+    }
+    
   return response_data
