d7a4d24701bfb64041132bd930065623e8321b73
SpawnageLoong <richardloongcj@gmail.com>
2024-04-10 13:51:33 +0700

    Fix parsing for JSON event body

--- fbMarketing/createFbCampaign.py
+++ fbMarketing/createFbCampaign.py
@@ -3,11 +3,22 @@ import requests
 import urllib.parse as urlparse
 
 def lambda_handler(event, context):
-  is_single = False
-  # Check if function is being triggered from another Lambda Function via API Gateway
-  if 'campaign_name' not in event:
-    is_single = True
-    event_body            = event['body']
+  event_body = event['body']
+
+  # Check if event body is in JSON form
+  if event_body.startswith('{'):
+    event_params          = json.loads(event_body)
+    campaign_name         = event_params['campaign_name']
+    campaign_objective    = event_params['campaign_objective']
+    campaign_buying_type  = event_params['campaign_buying_type']
+    campaign_status       = event_params['campaign_status']
+    special_ad_categories = event_params['special_ad_categories']
+    access_token          = event_params['access_token']
+    ad_account_id         = event_params['ad_account_id']
+    is_json               = True
+
+  # If not, then it is in URL-encoded form
+  else:
     event_params          = urlparse.parse_qs(event_body)
     campaign_name         = event_params['campaign_name'][0]
     campaign_objective    = event_params['campaign_objective'][0]
@@ -16,37 +27,31 @@ def lambda_handler(event, context):
     special_ad_categories = event_params['special_ad_categories'][0]
     access_token          = event_params['access_token'][0]
     ad_account_id         = event_params['ad_account_id'][0]
-  # If not, then it is being triggered from a Slack workflow
-  else:  
-    campaign_name = event['campaign_name']
-    campaign_objective = event['campaign_objective']
-    campaign_buying_type = event['campaign_buying_type']
-    campaign_status = event['campaign_status']
-    special_ad_categories = event['special_ad_categories']
-    access_token = event['access_token']
-    ad_account_id = event['ad_account_id']
+    is_json               = False
   
   if len(special_ad_categories) == 0:
     special_ad_categories = ["NONE"]
 
   form_data = {
-    'name': campaign_name,
-    'objective': campaign_objective,
-    'buying_type': campaign_buying_type,
-    'status': campaign_status,
+    'name'                 : campaign_name,
+    'objective'            : campaign_objective,
+    'buying_type'          : campaign_buying_type,
+    'status'               : campaign_status,
     'special_ad_categories': special_ad_categories,
-    'access_token': access_token
+    'access_token'         : access_token
   }
 
   url = f'https://graph.facebook.com/v19.0/{ad_account_id}/campaigns'
   response = requests.post(url, data=form_data)
   response_data = response.json()
     
-  if is_single:
+  # If the event body is in JSON form, return in a format for Slack
+  if is_json:
     return {
       "isBase64Encoded": False,
       "statusCode": 200,
       "body": json.dumps(response_data)
     }
     
+  # If the event body is in URL-encoded form, return in a format for Lambda Functions
   return response_data
