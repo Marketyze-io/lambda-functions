46bb7fcac3b6e01044dca24e9f8abf7a264d2bec
SpawnageLoong <richardloongcj@gmail.com>
2024-04-10 13:28:01 +0700

    Add event parsing for API gateway calls from other Lambda functions

--- fbMarketing/createFbCampaign.py
+++ fbMarketing/createFbCampaign.py
@@ -1,20 +1,30 @@
 import json
 import requests
+import urllib.parse as urlparse
 
 def lambda_handler(event, context):
   is_single = False
+  # Check if function is being triggered from another Lambda Function via API Gateway
   if 'campaign_name' not in event:
     is_single = True
-    event = json.loads(event['body'])
-    print("loading json...")
-    
-  campaign_name = event['campaign_name']
-  campaign_objective = event['campaign_objective']
-  campaign_buying_type = event['campaign_buying_type']
-  campaign_status = event['campaign_status']
-  special_ad_categories = event['special_ad_categories']
-  access_token = event['access_token']
-  ad_account_id = event['ad_account_id']
+    event_body            = event['body']
+    event_params          = urlparse.parse_qs(event_body)
+    campaign_name         = event_params['campaign_name'][0]
+    campaign_objective    = event_params['campaign_objective'][0]
+    campaign_buying_type  = event_params['campaign_buying_type'][0]
+    campaign_status       = event_params['campaign_status'][0]
+    special_ad_categories = event_params['special_ad_categories'][0]
+    access_token          = event_params['access_token'][0]
+    ad_account_id         = event_params['ad_account_id'][0]
+  # If not, then it is being triggered from a Slack workflow
+  else:  
+    campaign_name = event['campaign_name']
+    campaign_objective = event['campaign_objective']
+    campaign_buying_type = event['campaign_buying_type']
+    campaign_status = event['campaign_status']
+    special_ad_categories = event['special_ad_categories']
+    access_token = event['access_token']
+    ad_account_id = event['ad_account_id']
   
   if len(special_ad_categories) == 0:
     special_ad_categories = ["NONE"]
