d77d34de3a6bfec63a13a23e77e25fc1974a465d
SpawnageLoong <richardloongcj@gmail.com>
2024-04-10 14:10:32 +0700

    Add debugging print statements

--- fbMarketing/createFbCampaign.py
+++ fbMarketing/createFbCampaign.py
@@ -15,7 +15,6 @@ def lambda_handler(event, context):
     special_ad_categories = event_params['special_ad_categories']
     access_token          = event_params['access_token']
     ad_account_id         = event_params['ad_account_id']
-    is_json               = True
 
   # If not, then it is in URL-encoded form
   else:
@@ -27,7 +26,6 @@ def lambda_handler(event, context):
     special_ad_categories = event_params['special_ad_categories'][0]
     access_token          = event_params['access_token'][0]
     ad_account_id         = event_params['ad_account_id'][0]
-    is_json               = False
   
   if len(special_ad_categories) == 0:
     special_ad_categories = ["NONE"]
@@ -40,10 +38,13 @@ def lambda_handler(event, context):
     'special_ad_categories': special_ad_categories,
     'access_token'         : access_token
   }
+  print(f'form_data: {form_data}')
 
   url = f'https://graph.facebook.com/v19.0/{ad_account_id}/campaigns'
   response = requests.post(url, data=form_data)
+  print(f'response: {response.json()}')
   response_data = response.json()
+  print(f'response_data: {response_data}')
     
   return {
     "isBase64Encoded": False,
