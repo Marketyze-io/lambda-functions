eb954f7e59072aa786e484c2869f0f6986972704
SpawnageLoong <richardloongcj@gmail.com>
2024-04-17 17:22:43 +0700

    Add int typecasting in strings containing arithmetic

--- fbMarketing/update_gsheets_pull_only.py
+++ fbMarketing/update_gsheets_pull_only.py
@@ -169,7 +169,7 @@ def lambda_handler(event, context):
         gambling   = "TRUE" if "ONLINE_GAMBLING_AND_GAMING" in special_ad_categories else "FALSE"
 
         requestData = {
-            "range": f"'campaign-details'!A{str(lastRowNum+campaigns_added_count+1)}:L{str(lastRowNum+campaigns_added_count+2)}",
+            "range": f"'campaign-details'!A{str(int(lastRowNum)+campaigns_added_count+1)}:L{str(int(lastRowNum)+campaigns_added_count+2)}",
             "majorDimension": "ROWS",
             "values": [[campaign_name, campaign_id, campaign_objective_dropdown, campaign_objective, campaign_buying_type, campaign_status, credit, employment, housing, politics, gambling, special_ad_categories]]
         }
