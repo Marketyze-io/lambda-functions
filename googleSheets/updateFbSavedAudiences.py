19aa6f7b17f34709dfa08863b495bedfb1d1ec29
SpawnageLoong <richardloongcj@gmail.com>
2024-05-02 16:54:00 +0700

    Add function for updating saved audiences

---
+++ googleSheets/updateFbSavedAudiences.py
@@ -0,0 +1,42 @@
+import json
+import requests
+
+def lambda_handler(event, context):
+    event_params    = json.loads(event['body'])
+    print(event_params)
+    fb_access_token = event_params['fb_access_token']
+    ad_account_id   = event_params['ad_account_id']
+    gs_access_token = event_params['gs_access_token']
+    spreadsheet_id  = event_params['spreadsheet_id']
+
+    # Get the saved audiences from Facebook
+    saved_audiences_endpoint = f'https://graph.facebook.com/v19.0/{ad_account_id}/saved_audiences?limit=100&filtering=[{{"field": "name", "operator": "CONTAIN", "value": "Marketyze"}}]'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {fb_access_token}'
+    }
+    response = requests.get(saved_audiences_endpoint, headers=headers)
+    print(response.json())
+
+    # Append the saved audiences to Google Sheets
+    gs_append_payload = {
+        "range": "saved-audiences!A3",
+        "majorDimension": "ROWS",
+        "values": [],
+    }
+    for audience in response.json()['data']:
+        gs_append_payload['values'].append([audience['name'], audience['id']])
+    
+    # Clear the existing data in the sheet
+    gs_clear_endpoint = f'https://sheets.googleapis.com/v4/spreadsheets/{spreadsheet_id}/values/saved-audiences!A3:B:clear?access_token={gs_access_token}'
+    response = requests.post(gs_clear_endpoint)
+
+    # Append the new data
+    gs_append_endpoint = f'https://sheets.googleapis.com/v4/spreadsheets/{spreadsheet_id}/values/saved-audiences!A3:append?valueInputOption=USER_ENTERED&access_token={gs_access_token}'
+    response = requests.post(gs_append_endpoint, data=json.dumps(gs_append_payload))
+    print(response.status_code)
+    print(response.text)
+
+    return {
+        'statusCode': response.status_code
+    }
