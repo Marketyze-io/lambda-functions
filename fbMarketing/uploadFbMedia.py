cccf7bca912367d32ebe8b86f2698b006770b3c3
SpawnageLoong <richardloongcj@gmail.com>
2024-05-15 13:53:57 +0700

    Add boilerplate code to uploadFbMedia

--- fbMarketing/uploadFbMedia.py
+++ fbMarketing/uploadFbMedia.py
@@ -1,7 +1,44 @@
 import json
+import os
+import urllib.parse as urlparse
+import urllib.request
+import requests
+import datetime
+
+AWS_PARAM_STORE_ENDPOINT = "http://localhost:2773/systemsmanager/parameters/get/"
+SECRET_NAME = "/slack/fb-marketing/bot-oauth-token"
+aws_session_token = os.environ.get('AWS_SESSION_TOKEN')
+
+GOOGLE_DRIVE_ROOT_URL = 'https://www.googleapis.com/drive/v3/'
+
+SLACK_POST_MESSAGE_ENDPOINT = 'https://slack.com/api/chat.postMessage'
+
+def slack_post_message(channel_id, token, message):
+    slack_payload = {
+        'channel': channel_id,
+        'text': message
+    }
+    slack_request = requests.post(SLACK_POST_MESSAGE_ENDPOINT, headers
+        ={'Authorization': f'Bearer {token['Parameter']['Value']}'}, data=slack_payload)
+    print(slack_request.json())
 
 def lambda_handler(event, context):
-    # TODO implement
+    channel_id      = event['channel_id']
+    gs_access_token = event['gs_access_token']
+    spreadsheet_id  = event['spreadsheet_id']
+    fb_access_token = event['fb_access_token']
+    ad_account_id   = event['ad_account_id']
+    ad_account_name = event['ad_account_name']
+
+    # Get the token from AWS Parameter Store
+    secret_name = urlparse.quote(SECRET_NAME, safe="")
+    endpoint = f"{AWS_PARAM_STORE_ENDPOINT}?name={secret_name}&withDecryption=true"
+    req = urllib.request.Request(endpoint)
+    req.add_header('X-Aws-Parameters-Secrets-Token', aws_session_token)
+    token = urllib.request.urlopen(req).read()
+    token = json.loads(token)
+    print("Slack token retrieved")
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
